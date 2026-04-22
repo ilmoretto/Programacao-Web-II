932f58d95d6033d6c6e6094aad07602afc38e1f7
Alencar Ferreira Morete da Cruz <2024102060006@aluno.ifro.edu.br>
2026-04-22 19:56:16 -0400

    feat: configuração do banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './config/database/database.module';
 import { ProdutoModule } from './modules/produto/produto.module';
 
 @Module({
-  imports: [ProdutoModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    ProdutoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
